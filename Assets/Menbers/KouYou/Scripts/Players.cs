1ab669145968cc83fca366a57c2d94045aeff468
TangXingYe0125 <ttc2350dg0006@edu.tech.ac.jp>
2025-08-31 17:43:28 +0900

    1
    1

--- Assets/Menbers/KouYou/Scripts/Players.cs
+++ Assets/Menbers/KouYou/Scripts/Players.cs
@@ -9,16 +9,6 @@ public class Players : MonoBehaviour
     [SerializeField] private Object _superman;
     [SerializeField] private Vector3 _playerPos;
     [SerializeField] private Animator _playerGate;
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
-
     private IEnumerator Summon_Otaku()
     {
         _playerGate.SetBool("Open", true);
@@ -39,5 +29,18 @@ public class Players : MonoBehaviour
         Instantiate(_superman, _playerPos, Quaternion.identity);
         yield return new WaitForSeconds(0.5f);
         _playerGate.SetBool("Open", false);
+    }
+
+    public void StartSummon_Otaku()
+    {
+        StartCoroutine(Summon_Otaku());
+    }
+    public void StartSummon_Bro()
+    {
+        StartCoroutine(Summon_Bro());
+    }
+    public void StartSummon_SuperMan()
+    {
+        StartCoroutine(Summon_SuperMan());
     }
 }
