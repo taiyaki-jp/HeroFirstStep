43cfaa97d201c6bcf1feb31e05c6e742c29395a2
taiyaki-jp <ttc2350dg0001@edu.tech.ac.jp>
2025-08-31 22:44:20 +0900

    フォント、フェードパッケージ、プラグインフォルダの整理

---
+++ Assets/Plugins/FadePackage MadeBy taiyaki_jp/Scripts/FadeAndLoad.cs
@@ -0,0 +1,114 @@
+﻿using Cysharp.Threading.Tasks;
+using System;
+using FadeOrigins;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeAndLoad
+{
+    private float _fadeSpeed=1;
+    public float Speed
+    {
+        set => _fadeSpeed = value;
+    }
+    private Image _fadeImage;
+    public Image Image
+    {
+        set => _fadeImage = value;
+    }
+
+    /// <summary>
+    /// 各フェードを呼び出すUniTask
+    /// </summary>
+    /// <param name="mode">-1=フェードアウト　+1=フェードイン</param>
+    /// <param name="startColor">開始時の色</param>
+    /// <param name="endColor">終了時の色</param>
+    /// <param name="origin">[省略可]FillOriginEnumのどれか　省略すると透明度フェード</param>
+    /// <typeparam name="TOriginEnum"></typeparam>
+    public async UniTask FadeSystem<TOriginEnum>(int mode,Color startColor,Color endColor,TOriginEnum origin=default) where TOriginEnum : Enum
+    {
+
+        var useColor = (startColor != endColor);//フェード中色を変えるか
+
+        if (useColor==false)_fadeImage.color=startColor;
+        var useOrigin = origin!=null;//キャンバスを動かしてフェードするか
+
+        if (useOrigin)//ジェネリック変数をdefault(null)と比較
+        {
+            _fadeImage.fillMethod = AutoMethodSet(origin);
+            _fadeImage.fillOrigin = Convert.ToInt32(origin);//Enumをintに変換
+            if (mode == -1)
+                _fadeImage.fillAmount = 1;
+            else
+                _fadeImage.fillAmount = 0;
+        }
+        else
+        {
+            useOrigin = false;
+            _fadeImage.fillAmount = 1;
+        }
+
+        var t = 0f;
+        while (t<1)
+        {
+            t += _fadeSpeed * Time.deltaTime;
+            if (useOrigin) Fade(mode, t);
+            if (useColor)  Fade(t,startColor,endColor);
+            await UniTask.Yield();
+        }
+    }
+
+    /// <summary>
+    /// FillAmount式フェード
+    /// </summary>
+    /// <param name="mode">インかアウトか</param>
+    /// <param name="t">経過時間</param>
+    private void Fade(int mode,float t)
+    {
+        var fillAmount = 0f;
+        if (mode == -1)
+        {
+            fillAmount = 1 - t;
+        }
+        else if (mode == 1)
+        {
+            fillAmount = t;
+        }
+        _fadeImage.fillAmount = fillAmount;
+    }
+
+    /// <summary>
+    /// RGBAをいじる
+    /// </summary>
+    /// <param name="t">経過時間</param>
+    /// <param name="startColor">開始時の色</param>
+    /// <param name="endColor">終了時の色</param>
+    private void Fade( float t, Color startColor, Color endColor)
+    {
+        _fadeImage.color = Color.Lerp(startColor, endColor, t);
+    }
+
+
+    /// <summary>
+    /// 渡されたoriginに基づいてmethodを変える関数
+    /// </summary>
+    /// <typeparam name="TOriginEnum">FillOriginEnum.csの中のEnum</typeparam>
+    /// <param name="origin">渡されたOriginEnum</param>
+    private Image.FillMethod AutoMethodSet<TOriginEnum>(TOriginEnum origin)where TOriginEnum : Enum
+    {
+        switch (origin)
+        {
+            case Horizontal:
+                return Image.FillMethod.Horizontal;
+            case Vertical:
+                return Image.FillMethod.Vertical;
+            case Radial_90:
+                return Image.FillMethod.Radial90;
+            case Radial_180:
+                return Image.FillMethod.Radial180;
+            case Radial_360:
+                return Image.FillMethod.Radial360;
+        }
+        return Image.FillMethod.Horizontal;
+    }
+}
---
+++ Assets/Plugins/FadePackage MadeBy taiyaki_jp/Scripts/FadeManager.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using static FadeActionMode;//これがあるとAction設定のときにタイミング指定で補完が出るよ
+
+public class FadeManager : SingletonBase<FadeManager>
+{
+    [SerializeField, Header("フェード速度")] private float _fadeSpeed = 1;
+    [SerializeField] private GameObject _fadeCanvas;
+    private FadeAndLoad _load;
+
+    Action _beforeAction = null;
+    Action _afterAction = null;
+    Action _finishAction = null;
+
+    /// <summary>
+    /// 外部からActionを設定する
+    /// </summary>
+    /// <param name="timing">フェードのどのタイミングで実行するか</param>
+    /// <param name="action">設定する関数</param>
+    public void AddAction(FadeActionMode timing, Action action)
+    {
+        switch (timing)
+        {
+            case BeforeFade:
+                _beforeAction += action;
+                break;
+            case AfterFade:
+                _afterAction += action;
+                break;
+            case FinishFade:
+                _finishAction += action;
+                break;
+        }
+    }
+
+    /// <summary>
+    /// 外部から設定したActionを削除する
+    /// </summary>
+    /// <param name="timing">フェードのどのタイミングで実行するようにしたか</param>
+    /// <param name="action">削除する関数</param>
+    public void RemoveAction(FadeActionMode timing, Action action)
+    {
+        switch (timing)
+        {
+            case BeforeFade:
+                _beforeAction -= action;
+                break;
+            case AfterFade:
+                _afterAction -= action;
+                break;
+            case FinishFade:
+                _finishAction -= action;
+                break;
+        }
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();//SingletonBaseのAwakeを実行
+        _load = new FadeAndLoad
+        {
+            Image = _fadeCanvas.GetComponentInChildren<Image>(),
+            Speed = _fadeSpeed
+        };
+
+    }
+
+    private void Start()
+    {
+        _ = FirstFade();
+    }
+
+    /// <summary>
+    /// 最初のフェード
+    /// </summary>
+    private async UniTask FirstFade()
+    {
+        Debug.Log("FirstFade");
+        _afterAction?.Invoke();
+        await _load.FadeSystem<Enum>(-1, Color.black, Color.clear);
+        _finishAction?.Invoke();
+
+        _fadeCanvas.SetActive(false);
+    }
+
+
+    /// <summary>
+    /// フェードを呼び出す関数
+    /// </summary>
+    /// <param name="sceneName">遷移先のシーンの名前</param>
+    /// <param name="startOrigin">[省略可]FillOriginEnum.csのEnum 省略すると透明度フェード</param>
+    /// <param name="endOrigin">[省略可]FillOriginEnum.csのEnum 省略すると透明度フェード</param>
+    /// <param name="startColor">[省略可]フェード開始時の色　省略すると黒　透明度フェードなら透明</param>
+    /// <param name="midColor">[省略可]画面が見えなくなった時の色　省略すると黒</param>
+    /// <param name="midColor2">[省略可]画面が見えなくなったあと色をさらに変えたいときに使う</param>
+    /// <param name="endColor">[省略可]フェード終了時の色　省略すると黒　透明度フェードなら透明</param>
+    public async UniTask Fade<TOriginEnum>(string sceneName, TOriginEnum startOrigin = default, TOriginEnum endOrigin = default, Color startColor = default, Color midColor = default, Color midColor2 = default, Color endColor = default) where TOriginEnum : Enum
+    {
+        //defaultを変換
+        if (startColor == default) startColor = Color.black;//色省略なら黒に
+        if (startOrigin == null) startColor = new Color (startColor.r,startColor.g,startColor.b,0f);//origin省略なら透明に
+        
+
+        if (midColor == default) midColor = Color.black;//色省略なら黒に
+
+        if (endColor == default) endColor = Color.black;//色省略なら黒に
+        if (endOrigin == null) endColor = new Color(endColor.r, endColor.g, endColor.b, 0f);//origin省略なら透明に
+
+        _fadeCanvas.SetActive(true);
+        Color finalMid = midColor;
+
+        await _load.FadeSystem(+1, startColor, midColor, startOrigin);
+        _beforeAction?.Invoke();
+        if (midColor2 != default)
+        {
+            await UniTask.Delay(TimeSpan.FromSeconds(1f));
+            await _load.FadeSystem<Enum>(-1, midColor, midColor2);
+            await UniTask.Delay(TimeSpan.FromSeconds(1f));
+            finalMid = midColor2;
+        }
+
+        await SceneManager.LoadSceneAsync(sceneName);
+        _afterAction?.Invoke();
+
+        await _load.FadeSystem(-1, finalMid, endColor, endOrigin);
+        _finishAction?.Invoke();
+
+        _fadeCanvas.SetActive(false);
+    }
+}
